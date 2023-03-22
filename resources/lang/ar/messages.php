6f7b952c142e611bdcd664183267782716613974
Ahmed Mohamed Ibrahim <ahmed_noreldin@outlook.com>
2023-03-22 15:56:23 +0200

    translation en, ar

---
+++ resources/lang/ar/messages.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    "payment_failed" => "حدث خطأ اثناء تنفيذ العملية",
+    "payment_success" => "تمت عملية الدفع بنجاح",
+    "payment_cancelled" => "تم إلغاء عملية الدفع",
+    "payment_pending" => "عملية الدفع قيد الانتظار",
+    "balance_not_enough" => "رصيدك غير كافي لإتمام عملية الدفع",
+    "your_card_is_not_secured_with_3d_secure" =>
+        "بطاقتك غير مؤمنة بـ 3D Secure",
+    "incorrect_card_expiry_date" => "تاريخ انتهاء البطاقة غير صحيح",
+    "the_transaction_has_been_declined_by_your_bank" =>
+        "تم رفض العملية من قبل البنك",
+    "the_otp_number_was_entered_incorrectly" =>
+        "تم إدخال رقم OTP بشكل غير صحيح",
+    "An_error_occurred_while_executing_the_operation" =>
+        "حدث خطأ أثناء تنفيذ العملية",
+    "Your_card_is_not_authorized_with_3D_secure" =>
+        "لم يتم تأكيد رمز OTP بنجاح",
+];
---
+++ resources/lang/en/messages.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    "payment_failed" => "An error occurred while executing the operation",
+    "payment_success" => "Operation completed successfully",
+    "payment_cancelled" => "The payment operation has been cancelled",
+    "payment_pending" => "The payment operation is pending",
+    "balance_not_enough" =>
+        "Your balance is not enough to complete the payment operation",
+    "your_card_is_not_secured_with_3d_secure" =>
+        "Your card is not secured with 3D Secure",
+    "incorrect_card_expiry_date" => "Incorrect card expiry date",
+    "the_transaction_has_been_declined_by_your_bank" =>
+        "The transaction has been declined by your bank",
+    "the_otp_number_was_entered_incorrectly" =>
+        "The OTP number was entered incorrectly",
+    "An_error_occurred_while_executing_the_operation" =>
+        "An error occurred while executing the operation",
+    "Your_card_is_not_authorized_with_3D_secure" =>
+        "Your card is not authorized with 3D secure",
+];
