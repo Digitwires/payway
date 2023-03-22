28ecd0988036a0718cd6ab88b3853a1484a2080f
Ahmed Mohamed Ibrahim <ahmed_noreldin@outlook.com>
2023-03-22 15:55:04 +0200

    check required fields function

---
+++ src/Traits/SetRequiredFields.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Digitwires\Payments\Traits;
+
+use Digitwires\Payments\Exceptions\MissingInfoException;
+
+trait SetRequiredFields
+{
+    /**
+     * @param array $required_field
+     * @param string $gateway_name
+     * @throws MissingInfoException
+     */
+    public function checkRequiredFields(
+        array $required_field,
+        string $gateway_name
+    ): void {
+        $missing = [];
+        foreach ($required_field as $field) {
+            if (empty($this->$field)) {
+                $missing[] = $field;
+            }
+        }
+        if (count($missing) > 0) {
+            throw new MissingInfoException($gateway_name, $missing);
+        }
+    }
+}
