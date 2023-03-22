5e7e9c2acafc9a29b5d9efcb183c454c406ae1f4
Ahmed Mohamed Ibrahim <ahmed_noreldin@outlook.com>
2023-03-22 17:12:37 +0200

    update in paypal configuration

--- config/d-payway.php
+++ config/d-payway.php
@@ -4,10 +4,8 @@ return [
     "APP_NAME" => env("APP_NAME"),
 
     # PayPal Gateway Config
-    "PAYPAL_CLIENT_ID" => "YOUR_CLIENT_ID",
-    "PAYPAL_CLIENT_SECRET" => "YOUR_SECRET",
-    "PAYPAL_MODE" => "sandbox", // sandbox or live
-    "PAYPAL_CURRENCY" => "USD",
-    "PAYPAL_VERIFY_URL" => "https://ipnpb.paypal.com/cgi-bin/webscr",
-    "PAYPAL_CANCEL_URL" => "http://localhost:8000/cancel",
+    "PAYPAL_CLIENT_ID" => env("PAYPAL_CLIENT_ID"),
+    "PAYPAL_CLIENT_SECRET" => env("PAYPAL_CLIENT_SECRET"),
+    "PAYPAL_MODE" => env("PAYPAL_MODE", "sandbox"),
+    "PAYPAL_CURRENCY" => env("PAYPAL_CURRENCY", "USD"),
 ];
